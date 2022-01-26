0b6a64f320f56d7d2912f77d719ebe994d01dcf9
kingwangwong <kingngngco@gmail.com>
2022-01-26 10:27:41 -0600

    first commit

--- app.js
+++ app.js
@@ -1,69 +1,39 @@
-class Person {
-    //creates instance of Person with properties name, age, and numBooksRead
-    constructor(name, age, numBooksRead) {
-        this.name = name;
-        this.age = age;
-        this.numBooksRead = numBooksRead;
+class Shape {
+    constructor(color, sides, sideLength) {
+        this._color = color;
+        this._sides = sides;
+        this._sideLength = sideLength;
     }
-
-    //increments numBooksRead by 1
-    readNewBook() {
-        this.numBooksRead++;
+    getPerimeter() {
+        return this._sides * this._sideLength;
     }
 }
 
-class Electrician extends Person {
-    //creates instance of Electrician with Person properites plus certifications, an array of strings
-    constructor(name, age, numBooksRead, certifications) {
-        super(name, age, numBooksRead);
-        this.certifications = certifications;
+class Cube extends Shape {
+    constructor(color, sides, sideLength) {
+        super(color, sides, sideLength);
+    }
+    getVolume() {
+        return this._sides * this._sides * this._sides;
+    }
+    
+}
+
+class Prism extends Shape {
+    constructor(color, sides, sideLength, height) {
+        super(color, sides, sideLength);
+        this._height = height;
     }
 }
 
-class Teenager extends Person {
-    //creates instance of Teenager with Person properties plus isHungry, a boolean value
-    constructor(name, age, numBooksRead, isHungry = true) {
-        super(name, age, numBooksRead);
-        this.isHungry = isHungry;
-    }
+const blueHexagon = new Shape('blue', 6, 4);
+const redCube = new Cube('red', 4, 9);
+const yellowPentagonalPrism = new Prism('yellow', 5, 2, 10);
 
-    //feeds the teenager.  If they are hungry, changes isHungry to false, if they are not hungry, prints message to console.
-    eat() {
-        if(this.isHungry) {
-            this.isHungry = false;
-        } else {
-            console.log('Oh no I think I ate too much')
-        }
-    }
-}
+console.log(blueHexagon);
+console.log(redCube);
+console.log(yellowPentagonalPrism);
 
-//below we will test our classes by instantiating them, calling each method, and verifying output
-
-//create instances of each class
-let person = new Person('Euthyphro', 35, 0);
-let electrician = new Electrician('Zeus', 28, 3, ['Lightning', 'Polymorphism']);
-let teen = new Teenager('Persius', 16, 5, true);
-
-//log each instance
-console.log(person);
-console.log(electrician);
-console.log(teen);
-
-//call the method from the parent class to ensure it works as expected
-person.readNewBook();
-electrician.readNewBook();
-teen.readNewBook();
-
-//log the objects after calling .readNewBook() to verify it worked as intended
-console.log(person);
-console.log(electrician);
-console.log(teen);
-
-//call .eat()
-teen.eat();
-
-//check that .eat() when isHungry == true works as expected
-console.log(teen);
-
-//check that .eat() when isHungry == false works as expected
-teen.eat();
+console.log(blueHexagon.getPerimeter());
+console.log(redCube.getPerimeter());
+console.log(yellowPentagonalPrism.getPerimeter());
