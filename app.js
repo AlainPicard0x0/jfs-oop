819b28949fafb31a27e950411237e60fb53d14b5
f-safdar <techo.fatima@gmail.com>
2022-06-28 08:55:04 -0500

    Added oop

--- app.js
+++ app.js
@@ -67,3 +67,110 @@ console.log(teen);
 
 //check that .eat() when isHungry == false works as expected
 teen.eat();
+
+
+//Object Oriented Programming Exercise:
+
+
+// ----------- Parent class----------
+class Vehicle {
+// writing constructor creates instances of Vehicle
+    constructor(name, mode, types){
+        this._name = name;       
+        this._mode = mode;         
+        this._types = types;    
+        this._numOfWheels = 0;
+    }
+    get name() {
+        return this._name;
+    }
+    get mode() {
+        return this._mode;
+    }
+    get types() {
+        return this._types;
+    }
+    get numOfWheels(){
+        return this._numOfWheels;
+    }
+    set numOfWheels(wheels){
+        this._numOfWheels = wheels;
+    }
+        //add new items to the array
+    addModes(newMode){
+       this._mode.push(newMode);
+    }
+}
+
+//--------first child class----------
+class Car extends Vehicle {
+    constructor(name, mode, types, model){
+        super(name, mode, types);
+        this._model = model;            //one additional property
+    }
+    get model() {
+        return this._model;
+    }
+    //add new items to the array
+    addModel(newModel){                        // one additional method
+        this._model.push(newModel);
+        
+    }
+}
+
+//-----------second child class---------
+class bike extends Vehicle{
+    constructor(name, mode, types, yearOfInvent, numOfBikes){
+        super(name, mode, types);
+        this._yearOfInvent = yearOfInvent; 
+        this._numOfBikes = numOfBikes;       
+    }
+    get yearOfInvent(){
+        return this._yearOfInvent;
+    }
+    get numOfBikes(){
+        return this._numOfBikes;
+    }
+    //check if the year of invent , the bike is new or old
+    year(){                                    
+        if(this._yearOfInvent === 2022){
+            return true;
+        } else{
+            console.log(`This ${this._yearOfInvent} is old`);
+        }
+    }
+    //increments by 1 
+    totalBikes(){                               
+        this._numOfBikes++;  
+    }
+}
+
+//instances of parent Class
+
+let motorVehicle = new Vehicle('cars', ['road'], 'sedan', 4 );
+console.log(motorVehicle.name);
+//call function to add into array
+motorVehicle.addModes('air');
+motorVehicle.numOfWheels = 4;
+console.log(motorVehicle.numOfWheels);
+//log instance of parent class 
+console.log(motorVehicle);
+
+//instances of first Child class 
+
+let newCar = new Car('Lamborghini', ['road'], 'SUV', ['2022']);
+console.log(newCar.name);
+// call functions
+newCar.addModel('2021');
+//log instance of first child class 
+console.log(newCar);
+
+//instances of second Child class 
+
+let listBikes = new bike('Raven', ['road'], ['cruise'], '2020', 0);
+console.log(listBikes.name);
+//check that .year() is old or new 
+listBikes.year();
+listBikes.totalBikes();
+//log instance of second child class 
+console.log(listBikes);
